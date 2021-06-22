22af6fe3a4f951fa56f49279a253d5b85f85f372
Ravi <ravinder9.reddy@gmail.com>
2021-06-22 10:56:23 +0530

    Added create user, update user and jwt extension

--- src/app.py
+++ src/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from src.models import User, db
+import os
+from flask import Flask, request, jsonify, make_response
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, current_user
+from src.models import User, db, db_setup
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 app.config["JWT_SECRET_KEY"] = "super-secret"
+db_setup(app)
 jwt = JWTManager(app)
+
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
 
 
 @app.route("/", methods=["GET"])
@@ -12,34 +26,77 @@ def index():
     return "Welcome to Home page"
 
 
-@app.route('/api/users', methods=['POST'])
+# Only ADMIN can add users, so this API should be accessed only by ADMIN users.
+@app.route('/user', methods=['POST'])
 def new_user():
     username = request.json.get('username')
     password = request.json.get('password')
+    user = User.query.filter_by(name=username).first()
+    if user:
+        return jsonify({
+            "status": "fail",
+            "message": user.name + " already exists"
+        })
+
     user = User(name=username)
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
-    return "user"
+    return jsonify({
+        "user": username,
+        "status": "success"
+    })
 
 
-@app.route("/login", methods=["POST"])
+@app.route('/user', methods=['PATCH'])
+def update_user():
+    # username = request.json.get('username')
+    role = request.json.get('role')
+    user = User(role=role)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({
+        "message": "user updated"
+    })
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
-    if username != "test" or password != "test":
-        return jsonify({"msg": "Bad username or password"}), 401
+    user = User.query.filter_by(name=username).first()
+    if not user:
+        return make_response(
+            'Could not verify',
+            401,
+            {'WWW-Authenticate': 'Basic realm = "User does not exist !!"'}
+        )
+    if user.check_password(password):
+        access_token = create_access_token(identity=user)
+        return jsonify(access_token=access_token)
+    return make_response(
+        'Could not verify',
+        403,
+        {'WWW-Authenticate': 'Basic realm ="Wrong Password !!"'}
+    )
 
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
+
+@app.route('/logout')
+@jwt_required
+def logout():
+    return 'User logged out successfully'
 
 
-@app.route("/protected", methods=["GET"])
+@app.route("/who_am_i", methods=["GET"])
 @jwt_required()
 def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
+    # We can now access our sqlalchemy User object via `current_user`.
+    return jsonify(
+        id=current_user.id,
+        name=current_user.name,
+        role=current_user.role,
+        pwd=current_user.password_hash,
+    )
 
 
 if __name__ == "__main__":
--- src/models.py
+++ src/models.py
@@ -17,13 +17,14 @@ def db_setup(app, database_path=DATABASE_URL):
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     role = db.Column(db.String(10))
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password, method='sha256')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
