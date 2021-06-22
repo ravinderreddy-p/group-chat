691558cab6ca154e9dc0ff6cf92b0f65c8500829
Ravi <ravinder9.reddy@gmail.com>
2021-06-22 13:52:50 +0530

    Enabled JWT Blocklist by Redis

--- src/app.py
+++ src/app.py
@@ -1,11 +1,17 @@
 import os
+from datetime import timedelta
+
+import redis
 from flask import Flask, request, jsonify, make_response, abort
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, current_user
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, current_user, get_jwt
 from src.models import User, db, db_setup
+
+ACCESS_EXPIRES = timedelta(hours=1)
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
 db_setup(app)
 jwt = JWTManager(app)
 
@@ -19,6 +25,20 @@ def user_identity_lookup(user):
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
     return User.query.filter_by(id=identity).one_or_none()
+
+
+# Setup our redis connection for storing the blocklisted tokens
+jwt_redis_blocklist = redis.StrictRedis(
+    host="localhost", port=6379, db=0, decode_responses=True
+)
+
+
+# Callback function to check if a JWT exists in the redis blocklist
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+    token_in_redis = jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
 
 
 @app.route("/", methods=["GET"])
@@ -55,6 +75,7 @@ def new_user():
     })
 
 
+# Only ADMIN can add users, so this API should be accessed only by ADMIN users.
 @app.route('/user', methods=['PATCH'])
 @jwt_required()
 def update_user():
@@ -105,10 +126,12 @@ def login():
     )
 
 
-@app.route('/logout')
+@app.route("/logout", methods=["DELETE"])
 @jwt_required()
 def logout():
-    return 'User logged out successfully'
+    jti = get_jwt()["jti"]
+    jwt_redis_blocklist.set(jti, "", ex=ACCESS_EXPIRES)
+    return jsonify(msg="Access token revoked")
 
 
 @app.route("/who_am_i", methods=["GET"])
